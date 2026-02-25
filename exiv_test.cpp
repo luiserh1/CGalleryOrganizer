cbacb3eebe2ecebc5a6ae961caa85556eb7e38ea
Luis SH <luis.serranohernandez.1415@gmail.com>
2026-02-25 02:09:48 +0100

    chore: remove temporary exiv2 C++ test scratchpad

--- exiv_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <exiv2/exiv2.hpp>
-int main() {
-    auto image = Exiv2::ImageFactory::open("tests/assets/jpg/sample_deep_exif.jpg");
-    image->readMetadata();
-    auto& ed = image->exifData();
-    std::cout << ed["Exif.Image.Orientation"].toLong() << "\n";
-    return 0;
-}
