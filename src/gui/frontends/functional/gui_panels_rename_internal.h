4acb4546412a1fe7615611c6d9ad8ca0545cc7da
Luis SH <luis.serranohernandez.1415@gmail.com>
2026-03-04 02:38:49 +0100

    refactor(gui): split rename preview table renderer

---
+++ src/gui/frontends/functional/gui_panels_rename_internal.h
@@ -0,0 +1,8 @@
+#ifndef GUI_PANELS_RENAME_INTERNAL_H
+#define GUI_PANELS_RENAME_INTERNAL_H
+
+#include "gui/gui_common.h"
+
+int GuiRenameDrawPreviewTable(GuiUiState *state, Rectangle table_bounds);
+
+#endif // GUI_PANELS_RENAME_INTERNAL_H
